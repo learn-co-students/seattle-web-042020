dceb01944f11ff9b9d94539d0f86baf31564a273
Flatiron School <soundarya.rajendran@flatironschool.com>
2020-04-30 12:06:56 -0700

    Adding code

--- 12-orm-part-2/models/artist.rb
+++ 12-orm-part-2/models/artist.rb
@@ -9,7 +9,8 @@ class Artist
     # @@all << self
   end
 #***********************************************************************************************#
-# Delete -> drop table
+# Move all the data methods here
+# CRUD OPERATION
 def self.delete_table
   artist = CONN.execute("DROP TABLE artists")
 end
@@ -24,8 +25,6 @@ def self.all
     Artist.new(artist)
   end
 end 
-
-
 
 def self.update_name(id, old_name, new_name)
   artist = CONN.execute("UPDATE artists SET name = ? WHERE name = old_name AND id = ?", new_name, old_name, id)
@@ -47,6 +46,8 @@ def delete
   artist = CONN.execute("DELETE FROM artists WHERE name = ? && id = ?",name, id)
 end
 #***********************************************************************************************#
+#MOVE all the schema methods here
+# Label CRUD OPERATION
 
 def self.create_table(name)
   artist = CONN.execute("CREATE TABLE artists COLUMN(id INTEGER PRIMARY KEY, name TEXT)")
@@ -70,6 +71,24 @@ end
 
 def self.update_name(id, old_name, new_name)
   artist = CONN.execute("UPDATE artists SET name = ? WHERE name = old_name AND id = ?", new_name, old_name, id)
+end
+
+
+# Altering a table you are updating or creating
+#***********************************************************************************************#
+# Aggregation Methods Association Methods
+# artists class ( table ) -> need to talk to the albums (table)
+# find the matching artist_id
+# artist : id, name
+# album: id, title, artist_id
+# albums belongs to the artist[album_id: 1, album_id: 2, 3, 4,, 5, 6]
+# 
+def albums
+  albums = CONN.execute("
+    SELECT * FROM artists 
+    INNER JOIN albums
+    ON artists.id = albums.artist_id"
+  )
 end
 #***********************************************************************************************#
 end
