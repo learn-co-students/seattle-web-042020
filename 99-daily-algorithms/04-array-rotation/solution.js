34ce1f7537f0e31eef3baa42ef77c77b8aba9f79
hbj <heyhal19@gmail.com>
2020-06-15 14:21:53 -0700

    extra js solution

--- 99-daily-algorithms/04-array-rotation/solution.js
+++ 99-daily-algorithms/04-array-rotation/solution.js
@@ -1,4 +1,4 @@
-// Colton
+// Colton, O(n) -ish complexity
 
 //create a function 'rotate' that takes in an array and an integer
 //rotate each element to the left x number of times, x being the number provided in the second argument
@@ -11,3 +11,15 @@ function rotate(arr, num) {
     return arr
   }
   console.log(rotate([1,2,3,4,5], 2))
+
+
+  // O(1) complexity
+
+  function rotate(arr, num) {
+    num = num % arr.length
+    arr = arr.concat(arr.splice(0 , num))
+    return arr
+  }
+  arr = [1,2,3,4,5]
+  num = 2
+  console.log(rotate(arr,num))
