d69b989a6b9f307cdea70c26261ea13db01e904b
hbj <heyhal19@gmail.com>
2020-06-25 17:53:03 -0700

    one soltution

--- 99-daily-algorithms/10-count-and-say/solution.js
+++ 99-daily-algorithms/10-count-and-say/solution.js
@@ -1,1 +1,28 @@
 // Wheatley
+
+
+// Aidan
+
+function countAndSay(num) {
+    i = 1
+    let value = "1"
+    while (i < num) {
+      let temp = ""
+      let current = null
+      let count = null
+      for (j of value) {
+        if (current == j) {
+          count += 1
+        } else {
+          if (count) {
+            temp = temp + count + current
+          }
+          current = j
+          count = 1
+        }
+      }
+      value = temp + count + current
+      i++
+    }
+    return value
+}
