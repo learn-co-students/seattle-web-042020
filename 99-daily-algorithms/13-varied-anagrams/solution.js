a4621d6e2370724a0c13315ecdaafa6a93cf85cd
hbj <heyhal19@gmail.com>
2020-06-30 14:59:45 -0700

    anagram solutions

--- 99-daily-algorithms/13-varied-anagrams/solution.js
+++ 99-daily-algorithms/13-varied-anagrams/solution.js
@@ -1,1 +1,14 @@
 // Andy
+
+function randomGram(str){
+    let arr = str.split("")
+    let temp
+    for (i in arr) {
+      let charIndex = Math.floor(Math.random() * str.length)
+      temp = arr[charIndex]
+      arr[charIndex] = arr[i]
+      arr[i] = temp
+    }
+    return arr.join("")
+  }
+console.log(randomGram("pokebowl"))
