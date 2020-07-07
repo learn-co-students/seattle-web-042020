0510e665e884757d053730e16959fd43f4e21eab
hbj <heyhal19@gmail.com>
2020-07-07 16:48:48 -0700

    fixed typo

--- 99-daily-algorithms/12-valid-parens/solution.js
+++ 99-daily-algorithms/12-valid-parens/solution.js
@@ -1,15 +1,19 @@
 var isValid = function(s) {
     let dictionary = {"(":")", "{":"}", "[":"]"};
-    let stack = [")", "}"];
-//     str = "({)}"
+    let stack = [];
+    // s = "{]"
     
     for(let i=0;i<s.length;i++){
         let current = s[i]
         if(dictionary[current]){
+            // put matching closure bracket into stack
             stack.push(dictionary[current])
+            // stack now looks like this: ["}"]
         }
         else {
+            // we have found a closing bracket
             let match = stack.pop()
+            // match will be "}"
             if(match != current){
                 return false
             }
