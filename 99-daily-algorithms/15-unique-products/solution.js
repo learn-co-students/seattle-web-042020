957f42752787f4742d916f517a1680b248e6f246
hbj <heyhal19@gmail.com>
2020-07-09 15:26:23 -0700

    solutions 1

--- 99-daily-algorithms/15-unique-products/solution.js
+++ 99-daily-algorithms/15-unique-products/solution.js
@@ -1,1 +1,18 @@
 // Matt S
+
+
+// Aidan
+
+function uniqueProducts2(arr) {
+    let uniqueSet = new Set()
+    for (i of arr) {
+      i = `${i}`
+      let value = 1
+      for (j = 0; j < i.length; j++) {
+        console.log(i[j])
+        value = value * parseInt(i[j])
+      }
+      uniqueSet.add(value)
+    }
+    return uniqueSet.size
+  }
