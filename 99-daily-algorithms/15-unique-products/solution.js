4a5426bcc8bebe45e72cf9aa756ee05aedbcf248
hbj <heyhal19@gmail.com>
2020-07-13 08:52:19 -0700

    matt's solution

--- 99-daily-algorithms/15-unique-products/solution.js
+++ 99-daily-algorithms/15-unique-products/solution.js
@@ -1,5 +1,21 @@
 // Matt S
-
+function uniqueProducts(arr) {
+  let newArray = []
+    for (let i = 0; i < arr.length; i++) {
+          stringNumber = (`${arr[i]}`)
+          splitNumber = stringNumber.split('')
+          let product = 1
+          for (let k = 0; k < splitNumber.length; k++) {
+            product = product * parseInt(splitNumber[k])
+          }
+        if (newArray.includes(product)) {
+        } else {
+          newArray.push(product)
+        }
+    }
+    return newArray.length
+  }
+  uniqueProducts([9, 9, 99, 19191919191, 999, 111111111119])
 
 // Aidan
 
