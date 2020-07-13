d7407c73d2467c9ebc71ab38f98db1bb5f532ab8
hbj <heyhal19@gmail.com>
2020-07-13 16:41:54 -0700

    solutions

--- 99-daily-algorithms/16-candy-distribution/solutions.js
+++ 99-daily-algorithms/16-candy-distribution/solutions.js
@@ -1,1 +1,22 @@
-// Andy and Jennifer
+// Andy
+function candyDistrubition(n,m){
+    const divided = Math.floor(m/n)
+    const result = divided * n
+    return result
+}
+  //Don's solution
+function candyDistrubition1(n,m){
+const divided = ~~(m/n)
+const result = divided * n
+return result
+}
+  //John R's solution
+function candyDistrubition2(n,m){
+const divided = m%n
+const result = m - divided
+return result
+}
+
+
+
+// Jennifer
