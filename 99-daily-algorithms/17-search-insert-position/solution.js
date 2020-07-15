c1b193ef182fa6ede1e8c245d57e57a9da458dc8
hbj <heyhal19@gmail.com>
2020-07-14 17:02:20 -0700

    solutions

--- 99-daily-algorithms/17-search-insert-position/solution.js
+++ 99-daily-algorithms/17-search-insert-position/solution.js
@@ -1,3 +1,32 @@
-// Aidan
-
+// Colton
+function searchInsert(arr, val) {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] >= val) return i
+    }
+    return arr.length
+}
 // Justin
+
+// iterate through array
+// compare target to each element
+// - check to see if ===
+//     - if so, return that index #
+// - verify that target is greater than element
+//     - if so, continue iteration
+//     - if not, return current index #
+// var searchInsert = function(nums, target) {
+//     for (i=0; i < nums.length; i++) {
+//         if (nums[i] >= target) {
+//             return i
+//         } 
+//     }
+//     return nums.length
+// };
+const searchInsert = function(nums, target) {
+    for (const [i, v] of nums.entries()) {
+        if (target <= v) {
+            return i
+        }
+    }
+    return nums.length
+}
